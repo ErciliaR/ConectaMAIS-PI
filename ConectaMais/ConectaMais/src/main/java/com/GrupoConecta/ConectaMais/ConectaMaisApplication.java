22ec72dfefd1ce0aac7a4b576d65624d1de84deb
ErciliaR <72762687+ErciliaR@users.noreply.github.com>
2020-12-09 10:44:34 -0300

    Projeto Spring

---
+++ ConectaMais/ConectaMais/src/main/java/com/GrupoConecta/ConectaMais/ConectaMaisApplication.java
@@ -0,0 +1,13 @@
+package com.GrupoConecta.ConectaMais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaMaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaMaisApplication.class, args);
+	}
+
+}
---
+++ ConectaMais/ConectaMais/src/test/java/com/GrupoConecta/ConectaMais/ConectaMaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GrupoConecta.ConectaMais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaMaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
