0ee2f8bb492c56bfe3ac8649ef021607c03c91ef
Leonardo <you@example.com>
2021-01-21 19:57:38 -0300

    Subindo pasta assets e pagina de instituicao

---
+++ FrontEnd/instituicoes/instituicoes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-instituicoes',
+  templateUrl: './instituicoes.component.html',
+  styleUrls: ['./instituicoes.component.css']
+})
+export class InstituicoesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
