01deccca4218a1433348afccd7a2ac93e55074d5
Vitor <vitor.cutri@hotmail.com>
2021-01-25 10:12:51 -0300

    Página de cadastro

--- FrontEnd/ConectaMais/src/app/app.module.ts
+++ FrontEnd/ConectaMais/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
